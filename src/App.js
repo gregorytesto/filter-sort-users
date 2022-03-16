60f232712d4974039b5fabc107cb710420e3d3ac
Greg Testo <gregt@pursuit.org>
2022-03-16 12:13:31 -0400

    added sort and filter by name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import "./App.css";
 import users from "./data/users.js";
+import { useState } from "react";
 
 const App = () => {
-	let renderUsers = users.map((user, index) => {
-		return <li key={index}>{user.name}</li>;
+	const [searchInput, setSearchInput] = useState("");
+	const [sortInput, setSortInput] = useState("");
+	// Filter users
+	let filteredUsers = users.filter((user) => {
+		let lowerCaseName = user.name.toLowerCase();
+		let lowerCaseSearchInput = searchInput.toLowerCase();
+		return lowerCaseName.includes(lowerCaseSearchInput);
 	});
+
+	if (sortInput) {
+		filteredUsers.sort((a, b) => {
+			if (a[sortInput] < b[sortInput]) {
+				return -1;
+			}
+			if (a[sortInput] > b[sortInput]) {
+				return 1;
+			}
+			return 0;
+		});
+	}
+
+	let renderUsers = filteredUsers.map((user, index) => {
+		return (
+			<li key={index}>
+				{user.name} - {user.currency}
+			</li>
+		);
+	});
+
+	const handleSearchInput = (e) => {
+		setSearchInput(e.target.value);
+	};
+	const handleSortOnChange = (e) => {
+		setSortInput(e.target.value);
+	};
 	return (
 		<div>
 			<h1>Users Filter Sort</h1>
+			<form>
+				<label htmlFor="search_input">Search Name: </label>
+				<input id="search_input" type="text" value={searchInput} onChange={handleSearchInput} />
+
+				{/* <label>Sort</label> */}
+				<select onChange={handleSortOnChange}>
+					<option>--Sort--</option>
+					<option value="name">Name</option>
+					<option value="currency">Currency</option>
+				</select>
+			</form>
 			<ul>{renderUsers}</ul>
 		</div>
 	);
